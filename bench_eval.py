c1cd0a1e930adfacdb252c1a5f2bdc6729dc8c6f
Joe Dewar <joe@dakotandt.com>
2026-08-21 10:59:31 -0700

    chore(bench): make bench_eval print statement universal

--- bench_eval.py
+++ bench_eval.py
@@ -4,7 +4,7 @@ def main():
     alpha = float(sys.argv[1]) if len(sys.argv) > 1 else 1.0
     beta = float(sys.argv[2]) if len(sys.argv) > 2 else 1.0
     loss = (alpha - 0.5)**2 + (beta - 0.2)**2
-    print(f"[OPTIMIZER_METRIC]: {loss}")
+    print("[OPTIMIZER_METRIC]: " + str(loss))
 
 if __name__ == "__main__":
     main()
